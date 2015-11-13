b4604b57f309c786eb1274672071b41caa9bad16
hxf <243843598@qq.com>
2015-11-13 18:16:24 +0800

    add new fuctions:switch cityies and update weather

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -82,14 +82,23 @@ public class ChooseAreaActivity extends Activity {
 	 * 当前选中的级别
 	 */
 	private int currentLevel;
+	
+	/*
+	 * 是否从WeatherActivity中跳转过来
+	 */
+	private boolean isFromWeaterActivity;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		
+		isFromWeaterActivity = getIntent().getBooleanExtra("from_weather_activity", false);
+		
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		if (prefs.getBoolean("city_selected", false)) {
+		
+		//已经选择了城市切不是从WeatherActivity跳转过来，才会直接跳转到WeatherActivity
+		if (prefs.getBoolean("city_selected", false)&&!isFromWeaterActivity) {
 			Intent intent = new Intent(this, WeatherAcitivty.class);
 			startActivity(intent);
 			finish();
@@ -243,6 +252,10 @@ public class ChooseAreaActivity extends Activity {
 		} else if (currentLevel == LEVEL_CITY) {
 			queryProvinces();
 		} else {
+			if (isFromWeaterActivity) {
+				Intent intent = new Intent(this, WeatherAcitivty.class);
+				startActivity(intent);
+			}
 			finish();
 		}
 	}
